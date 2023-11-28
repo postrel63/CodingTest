02fc3b19d078a46f247829d916de9bb0636ab3fa
postrel63 <postrel63@naver.com>
2023-11-28 23:55:19 +0900

    11번

---
+++ inFlearn_kim/Main1_11/Main2.java
@@ -0,0 +1,33 @@
+package inFlearn_kim.Main1_11;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        System.out.println(Solution(str));
+    }
+
+    private static String Solution(String str) {
+        int count = 1;
+        char[] strList = str.toCharArray();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length() - 1; i++) {
+            if (strList[i] == strList[i + 1]) {
+                count++;
+            } else {
+                    sb.append(strList[i]);
+                if (count > 1) {
+                    sb.append(count);
+                }
+                count = 1;
+            }
+
+        }
+
+
+        return sb.toString();
+    }
+}
