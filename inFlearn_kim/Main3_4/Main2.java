ee7b83e9854441ea05df710024678c310c0c6769
postrel63 <postrel63@naver.com>
2023-12-22 23:45:27 +0900

    3-4

---
+++ inFlearn_kim/Main3_4/Main2.java
@@ -0,0 +1,41 @@
+package inFlearn_kim.Main3_4;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int M= scan.nextInt();
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = scan.nextInt();
+        }
+        System.out.println(Solution(N,M,arr));
+
+    }
+    public static int Solution(int N, int M, int[] arr){
+
+        int lt = 0, rt = 0;
+        int sum = 0;
+        int count =0;
+
+      for (rt = 0; rt < N ; rt ++ ){
+          sum += arr[rt];
+          if (sum == M){
+              count++;
+          }
+          while(sum >= M){
+              sum -= arr[lt++];
+              if (sum == M){
+                  count++;
+              }
+          }
+
+      }
+
+
+
+        return count;
+    }
+}
