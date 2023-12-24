8c1e594edc229acff38bade20155c402be5b63de
postrel63 <postrel63@naver.com>
2023-12-24 23:39:00 +0900

    6-3

---
+++ inFlearn_kim/Main6_3/Main2.java
@@ -0,0 +1,33 @@
+package inFlearn_kim.Main6_3;
+
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = scan.nextInt();
+        }
+        for (int x : Solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+
+    private static int[] Solution(int n, int[] arr) {
+
+        for (int i = 1; i < n; i++) {
+            int temp = arr[i], j;
+            for (j = i - 1; j >= 0; j--) {
+                if (arr[j] > temp) {
+                    arr[j+1] = arr[j];
+                } else break;
+            }
+            arr[j+1] = temp;
+
+        }
+        return arr;
+    }
+}
+
