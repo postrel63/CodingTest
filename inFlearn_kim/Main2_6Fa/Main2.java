5c7807c80a59d47c2c4556fa48ad3cb04216659e
postrel63 <postrel63@naver.com>
2023-12-25 21:47:19 +0900

    2-6

---
+++ inFlearn_kim/Main2_6Fa/Main2.java
@@ -0,0 +1,52 @@
+package inFlearn_kim.Main2_6Fa;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = scan.nextInt();
+        }
+        for (int x : solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+
+    }
+
+    public static ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            int temp = arr[i];
+            int res = 0;
+            while (temp > 0) {
+                int t = temp % 10;
+                res = res * 10 + t;
+                temp = temp / 10;
+
+
+            }
+            if (isPrime(res)) {
+                list.add(res);
+            }
+        }
+
+        return list;
+    }
+
+    public static boolean isPrime(int res) {
+        if (res == 1) {
+            return false;
+        }
+        for (int i = 2; i < res; i++) {
+            if (res % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
