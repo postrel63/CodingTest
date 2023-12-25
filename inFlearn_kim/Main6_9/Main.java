a33e16e7d16d9d9aa07d47c970ae37fd9d4d1ea5
postrel63 <postrel63@naver.com>
2023-12-25 21:46:44 +0900

    6-9 decision algorithm

---
+++ inFlearn_kim/Main6_9/Main.java
@@ -0,0 +1,26 @@
+package inFlearn_kim.Main6_9;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        int M = scan.nextInt();
+
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = scan.nextInt();
+        }
+        System.out.println(solution(N,M,arr));
+    }
+
+    private static int solution(int n, int m, int[] arr) {
+        int result = 0;
+
+
+
+
+        return result;
+    }
+}
