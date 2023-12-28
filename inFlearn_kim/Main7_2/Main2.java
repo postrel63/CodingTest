ac9e11322bafaaa8a05ae5494b3c239d1aae3d10
postrel63 <postrel63@naver.com>
2023-12-28 23:59:38 +0900

    7-2

---
+++ inFlearn_kim/Main7_2/Main2.java
@@ -0,0 +1,40 @@
+package inFlearn_kim.Main7_2;
+
+//재귀함수를 이용한 2진수 출력
+
+public class Main2 {
+    public static void main(String[] args) {
+
+//        DFS(20);
+//        int result = Fact(10);
+        System.out.println("---");
+        Fibo(10);
+
+    }
+
+    public static void DFS(int N) {
+        if (N == 0) return;
+
+        DFS(N / 2);
+        System.out.print(N % 2);
+    }
+
+    public static int Fact(int N) {
+        if (N == 1) return 1;
+
+        return N * Fact(N - 1);
+    }
+
+    public static int Fibo(int N) {
+        if (N == 1) return 1;
+        else if (N == 2) {
+            return 1;
+        } else {
+                    int n = Fibo(N - 2) + Fibo(N - 1);
+            System.out.println(n);
+            return n;
+        }
+
+
+    }
+}
