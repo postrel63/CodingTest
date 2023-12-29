c259b10b6728fe028ba2c3f4dfcc3d37c791563b
postrel63 <postrel63@naver.com>
2023-12-29 23:27:40 +0900

    7-6

---
+++ inFlearn_kim/Main7_6/Main2.java
@@ -0,0 +1,33 @@
+package inFlearn_kim.Main7_6;
+
+
+public class Main2 {
+
+    public static boolean[] check;
+    static int n;
+
+    public static void main(String[] args) {
+
+        n = 3;
+        check = new boolean[n + 1];
+        DFS(1);
+
+    }
+
+    public static void DFS(int idx) {
+       if (idx == n+1){
+           for (int i = 1; i < idx; i++) {
+               if (check[i] == true){
+                   System.out.print(i+ " ");
+               }
+           }
+               System.out.println();
+           return;
+       }
+       check[idx] = true;
+       DFS(idx+1);
+       check[idx] = false;
+       DFS(idx+1);
+    }
+
+}
