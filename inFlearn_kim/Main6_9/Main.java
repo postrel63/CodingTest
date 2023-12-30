f6b209e27a18a1c13d8140b9435c4d1721d39d1f
postrel63 <postrel63@naver.com>
2023-12-30 20:38:31 +0900

    decision algorithm

--- inFlearn_kim/Main6_9/Main.java
+++ inFlearn_kim/Main6_9/Main.java
@@ -1,6 +1,8 @@
 package inFlearn_kim.Main6_9;
 
+import java.util.Arrays;
 import java.util.Scanner;
+//결정 알고리즘
 
 public class Main {
     public static void main(String[] args) {
@@ -17,10 +19,34 @@ public class Main {
 
     private static int solution(int n, int m, int[] arr) {
         int result = 0;
+        int lt = Arrays.stream(arr).max().getAsInt(); // getAsInt 형변환
+        int rt = Arrays.stream(arr).sum(); // sum은 형변환 상관없음
 
+        while (lt <= rt){
+            int mid = lt + (rt - lt) / 2;
 
-
-
+            if (count(arr,mid) <= m){
+                result = mid;
+                rt = mid-1;
+            }else{
+                lt = mid+1;
+            }
+        }
         return result;
+    }
+
+
+    public static int count(int[] arr, int capacity){
+        int cnt = 1;
+        int sum = 0;
+        for(int x : arr){
+            if(sum+x > capacity){
+                cnt++;
+                sum=x;
+            }else{
+                sum+=x;
+            }
+        }
+        return cnt;
     }
 }
