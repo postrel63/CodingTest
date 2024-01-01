4bec7103374663fbfcc4e2b2118216e7f40e7e89
postrel63 <postrel63@naver.com>
2024-01-01 16:27:49 +0900

    .

--- inFlearn_kim/Main7_5_DFS/Main2.java
+++ inFlearn_kim/Main7_5_DFS/Main2.java
@@ -24,13 +24,13 @@ public class Main2 {
 
     public static void main(String[] args) {
         Main tree = new Main();
-        tree.root = new Node2(1);
-        tree.root.lt = new Node2(2);
-        tree.root.rt = new Node2(3);
-        tree.root.lt.lt = new Node2(4);
-        tree.root.lt.rt = new Node2(5);
-        tree.root.rt.lt = new Node2(6);
-        tree.root.rt.rt = new Node2(7);
+        tree.root = new Node(1);
+        tree.root.lt = new Node(2);
+        tree.root.rt = new Node(3);
+        tree.root.lt.lt = new Node(4);
+        tree.root.lt.rt = new Node(5);
+        tree.root.rt.lt = new Node(6);
+        tree.root.rt.rt = new Node(7);
         tree.DFS(tree.root);
 
     }
